e80b511969f9fd1e0c022f80eff8d9f9bd791135
KitsuneNoctus <theceintiant@gmail.com>
2023-08-09 11:32:25 -0700

    Obj-C Fcuntions 3 - Changing the Text Properties

--- ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.h
+++ ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.h
@@ -16,5 +16,15 @@
 
 - (IBAction)enterText:(id)sender;
 
+- (IBAction)setColor:(id)sender;
+- (IBAction)setBackground:(id)sender;
+- (IBAction)fontSize:(id)sender;
+- (IBAction)dropShadow:(id)sender;
+- (IBAction)shadowColor:(id)sender;
+- (IBAction)center:(id)sender;
+- (IBAction)left:(id)sender;
+- (IBAction)right:(id)sender;
+
+
 @end
 
--- ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.m
+++ ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.m
@@ -22,12 +22,63 @@
   
 }
 
-
 - (IBAction)enterText:(id)sender {
   
   self.label.text = self.textField.text;
   [self resignFirstResponder]; // This is for iOS version below a certain version
 }
+
+
+- (IBAction)right:(id)sender {
+  self.label.textAlignment = NSTextAlignmentRight;
+}
+
+- (IBAction)left:(id)sender {
+  self.label.textAlignment = NSTextAlignmentLeft;
+}
+
+- (IBAction)center:(id)sender {
+  
+  self.label.textAlignment = NSTextAlignmentCenter;
+  
+}
+
+
+- (IBAction)shadowColor:(id)sender {
+  
+  self.label.layer.shadowColor = [[UIColor blueColor] CGColor];
+  
+}
+
+- (IBAction)dropShadow:(id)sender {
+  
+  self.label.layer.shadowColor = [[UIColor blackColor] CGColor];
+  self.label.layer.shadowOpacity = 0.5;
+  self.label.layer.shadowRadius = 1.0f;
+  self.label.layer.shadowOffset = CGSizeMake(2, 2);
+  
+}
+
+- (IBAction)fontSize:(id)sender {
+  
+  [self.label setFont:[UIFont fontWithName:@"Futura" size:30]];
+  
+}
+
+- (IBAction)setBackground:(id)sender {
+  
+  self.label.backgroundColor = [UIColor blackColor];
+  
+}
+
+- (IBAction)setColor:(id)sender {
+
+  self.label.textColor = [UIColor redColor];
+  
+}
+
+
+
 
 -(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
   
