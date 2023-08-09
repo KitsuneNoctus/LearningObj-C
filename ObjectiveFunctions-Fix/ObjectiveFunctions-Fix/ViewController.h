c7fea1518bc02c82857ea6ff244bd5f7a693832e
KitsuneNoctus <theceintiant@gmail.com>
2023-08-09 11:08:08 -0700

    Obj-C Fcuntions 2 - Dismissing keyboard

--- ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.h
+++ ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.h
@@ -7,7 +7,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
--- ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.m
+++ ObjectiveFunctions-Fix/ObjectiveFunctions-Fix/ViewController.m
@@ -26,5 +26,19 @@
 - (IBAction)enterText:(id)sender {
   
   self.label.text = self.textField.text;
+  [self resignFirstResponder]; // This is for iOS version below a certain version
 }
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+  
+  if ([text rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].location == NSNotFound) {
+    return YES;
+  }
+  
+  [textView resignFirstResponder];
+  
+  return NO;
+  
+}
+
 @end
