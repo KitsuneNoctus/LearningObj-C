5f04a23af42f72285d21f152f82e61daba705af0
KitsuneNoctus <theceintiant@gmail.com>
2023-08-14 12:10:32 -0700

    Obj-C Fcuntions 14 - Tap Me Fast Game part 2

--- My Tap Me Fast/My Tap Me Fast/ViewController.m
+++ My Tap Me Fast/My Tap Me Fast/ViewController.m
@@ -16,9 +16,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    timeInt = 10;
+    tapInt = 0;
+    gameInt = 0;
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", tapInt];
+    
+    
 }
 
 
 - (IBAction)startGame:(id)sender {
+    
+    if (timeInt == 10) {
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startCounter) userInfo:nil repeats:YES];
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.5;
+    }
+    
+    if (gameInt == 1) {
+        tapInt += 1;
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i", tapInt];
+    }
+    else {
+        timeInt = 10;
+        tapInt = 0;
+        
+        self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i", tapInt];
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    
 }
+
+-(void)startCounter {
+    timeInt -= 1;
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+    
+    gameInt = 1;
+    
+    self.startButton.enabled = YES;
+    self.startButton.alpha = 1.0;
+    
+    [self.startButton setTitle:@"Tap" forState:UIControlStateNormal];
+    
+    if (timeInt == 0) {
+        [timer invalidate];
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.5;
+        
+        [self.startButton setTitle:@"Restart" forState:UIControlStateNormal];
+        
+        [NSTimer scheduledTimerWithTimeInterval:4.0 target:self selector:@selector(restart) userInfo:nil repeats:NO];
+    }
+}
+
+-(void)restart {
+    self.startButton.enabled = YES;
+    self.startButton.alpha = 1.0;
+    
+    gameInt = 0;
+}
+
 @end
