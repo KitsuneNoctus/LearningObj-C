04cb88a080b823f0f99c8b088385deba36b7130c
KitsuneNoctus <theceintiant@gmail.com>
2023-09-08 10:33:02 -0700

    Obj-C Views 9 - Using UIPickerViews START

---
+++ Section 6 - Views/UIPickerViews/UIPickerViews/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  UIPickerViews
+//
+//  Created by Henry Calderon on 9/8/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Section 6 - Views/UIPickerViews/UIPickerViews/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UIPickerViews
+//
+//  Created by Henry Calderon on 9/8/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
