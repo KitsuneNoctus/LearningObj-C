0dde7241d1201f1e5d785239f312f21388df7809
KitsuneNoctus <theceintiant@gmail.com>
2023-09-18 12:08:47 -0700

    Obj-C Views 11 - Web View Application START P1

---
+++ Section 6 - Views/WebViewApplication/WebViewApplication/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WebViewApplication
+//
+//  Created by Henry Calderon on 9/18/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Section 6 - Views/WebViewApplication/WebViewApplication/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WebViewApplication
+//
+//  Created by Henry Calderon on 9/18/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
