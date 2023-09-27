62d9573c6526f9f224805cd3c48db0fb1c0bde10
KitsuneNoctus <theceintiant@gmail.com>
2023-09-27 15:15:51 -0700

    Obj-C Visuals - 3 - Mini Image Gallery Application END

--- Section 6 - Views/MyImageGallery/MyImageGallery/ViewController.h
+++ Section 6 - Views/MyImageGallery/MyImageGallery/ViewController.h
@@ -7,7 +7,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    int imageInt;
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+
+- (IBAction)back:(id)sender;
+- (IBAction)next:(id)sender;
+
 
 
 @end
--- Section 6 - Views/MyImageGallery/MyImageGallery/ViewController.m
+++ Section 6 - Views/MyImageGallery/MyImageGallery/ViewController.m
@@ -16,7 +16,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    imageInt = 1;
+    [self imageGallery];
 }
 
 
+- (IBAction)next:(id)sender {
+    imageInt += 1;
+    [self imageGallery];
+}
+
+- (IBAction)back:(id)sender {
+    imageInt -= 1;
+    [self imageGallery];
+}
+
+-(void)imageGallery {
+    self.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"Image%i.jpg", imageInt]];
+    self.label.text = [NSString stringWithFormat:@"%i/6", imageInt];
+    
+    if (imageInt == 1) {
+        self.backButton.enabled = NO;
+        self.backButton.alpha = 0.5;
+    }
+    else if (imageInt == 6) {
+        self.nextButton.enabled = NO;
+        self.nextButton.alpha = 0.5;
+    }
+    else {
+        self.backButton.enabled = YES;
+        self.backButton.alpha = 1.0;
+        self.nextButton.enabled = YES;
+        self.nextButton.alpha = 1.0;
+    }
+}
 @end
