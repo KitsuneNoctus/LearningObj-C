65cf50acb1529c0af12b22d4a07a9de375273d42
KitsuneNoctus <theceintiant@gmail.com>
2023-09-27 15:40:15 -0700

    Obj-C Visuals - 5 - Displaying Images From URL's START

---
+++ Section 6 - Views/ImagesFromURL/ImagesFromURL/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ImagesFromURL
+//
+//  Created by Henry Calderon on 9/27/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Section 6 - Views/ImagesFromURL/ImagesFromURL/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ImagesFromURL
+//
+//  Created by Henry Calderon on 9/27/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
