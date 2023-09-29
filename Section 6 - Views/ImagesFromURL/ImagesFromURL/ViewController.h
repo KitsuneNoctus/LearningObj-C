b3cab25014e5e7d42010179494c8c927b124f85c
KitsuneNoctus <theceintiant@gmail.com>
2023-09-29 10:58:55 -0700

    Obj-C Visuals - 5 - Displaying Images From URL's END

--- Section 6 - Views/ImagesFromURL/ImagesFromURL/ViewController.h
+++ Section 6 - Views/ImagesFromURL/ImagesFromURL/ViewController.h
@@ -9,6 +9,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 
 @end
 
--- Section 6 - Views/ImagesFromURL/ImagesFromURL/ViewController.m
+++ Section 6 - Views/ImagesFromURL/ImagesFromURL/ViewController.m
@@ -16,6 +16,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://img.freepik.com/free-photo/red-white-cat-i-white-studio_155003-13189.jpg?t=st=1696009695~exp=1696010295~hmac=dc16820a49e152682aa483cd0ffce74667369e7c2327c5644e4125ec5f52336a"]]];
 }
 
 
