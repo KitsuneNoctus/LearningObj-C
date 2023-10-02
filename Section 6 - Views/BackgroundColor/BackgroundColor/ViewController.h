358465e9486b8d4637a7a1de6e7f8238f692e63f
KitsuneNoctus <theceintiant@gmail.com>
2023-10-02 16:59:52 -0700

    Obj-C Visuals - 9 - Changing the Background Color END

--- Section 6 - Views/BackgroundColor/BackgroundColor/ViewController.h
+++ Section 6 - Views/BackgroundColor/BackgroundColor/ViewController.h
@@ -9,6 +9,16 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)red:(id)sender;
+- (IBAction)blue:(id)sender;
+- (IBAction)green:(id)sender;
+- (IBAction)yellow:(id)sender;
+- (IBAction)black:(id)sender;
+- (IBAction)white:(id)sender;
+- (IBAction)custom:(id)sender;
+
+
+
 
 @end
 
--- Section 6 - Views/BackgroundColor/BackgroundColor/ViewController.m
+++ Section 6 - Views/BackgroundColor/BackgroundColor/ViewController.m
@@ -19,4 +19,31 @@
 }
 
 
+- (IBAction)custom:(id)sender {
+    self.view.backgroundColor = [UIColor colorWithRed:128.0/255.0 green:56.0/255.0 blue:241.0/255.0 alpha:1.0];
+}
+
+- (IBAction)white:(id)sender {
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (IBAction)black:(id)sender {
+    self.view.backgroundColor = [UIColor blackColor];
+}
+
+- (IBAction)yellow:(id)sender {
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+- (IBAction)green:(id)sender {
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+- (IBAction)blue:(id)sender {
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (IBAction)red:(id)sender {
+    self.view.backgroundColor = [UIColor redColor];
+}
 @end
