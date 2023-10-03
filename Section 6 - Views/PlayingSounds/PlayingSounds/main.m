836fdb5ee714d07f82385c40caa3f4515384a48c
KitsuneNoctus <theceintiant@gmail.com>
2023-10-03 16:39:16 -0700

    Obj-C Audio & Video - 1 - Playing a Sound File START

---
+++ Section 6 - Views/PlayingSounds/PlayingSounds/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayingSounds
+//
+//  Created by Henry Calderon on 10/3/23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
