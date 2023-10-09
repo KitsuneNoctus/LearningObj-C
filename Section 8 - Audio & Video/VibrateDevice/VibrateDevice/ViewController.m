f1ced35b2c7742ba786a5ea7cc97532a5032badf
KitsuneNoctus <theceintiant@gmail.com>
2023-10-09 16:32:49 -0700

    Obj-C Audio & Video - 6 - Vibrating the Device START

---
+++ Section 8 - Audio & Video/VibrateDevice/VibrateDevice/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  VibrateDevice
+//
+//  Created by Henry Calderon on 10/9/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
