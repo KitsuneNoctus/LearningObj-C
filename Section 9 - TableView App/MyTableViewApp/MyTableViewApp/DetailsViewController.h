d4cbf87130d70e1a0016fc5e953134da31f2c822
KitsuneNoctus <theceintiant@gmail.com>
2023-10-17 11:24:03 -0700

    Obj-C TableView - 3 - London Landmarks - Populating a Detail View Part 3

--- Section 9 - TableView App/MyTableViewApp/MyTableViewApp/DetailsViewController.h
+++ Section 9 - TableView App/MyTableViewApp/MyTableViewApp/DetailsViewController.h
@@ -6,12 +6,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
 
 @property(strong, nonatomic) NSArray *detailModal;
+
+@property (weak, nonatomic) IBOutlet UIImageView *detailImageView;
+@property (weak, nonatomic) IBOutlet UILabel *detailTitle;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescription;
+@property (weak, nonatomic) IBOutlet UITextView *detailTextView;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+- (IBAction)directions:(id)sender;
+
+
 
 
 @end
--- Section 9 - TableView App/MyTableViewApp/MyTableViewApp/DetailsViewController.m
+++ Section 9 - TableView App/MyTableViewApp/MyTableViewApp/DetailsViewController.m
@@ -17,6 +17,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.navigationItem.title = self.detailModal[0];
+    
+    self.detailTitle.text = self.detailModal[0];
+    self.detailDescription.text = self.detailModal[1];
+    self.detailImageView.image = [UIImage imageNamed:self.detailModal[2]];
+    
+    self.button.layer.cornerRadius = 5;
+    self.mapView.layer.cornerRadius = 5;
+    
+    if ([self.detailTitle.text isEqualToString:@"Big Ben"]) {
+        self.detailTextView.text = @"Big Ben is the nickname for the Great Bell of the Great Clock of Westminster, at the north end of the Palace of Westminster in London, England, and the name is frequently extended to refer also to the clock and the clock tower.";
+    }
+    if ([self.detailTitle.text isEqualToString:@"Buckingham Palace"]) {
+        self.detailTextView.text = @"Buckingham Palace is a royal residence in London and the administrative headquarters of the monarch of the United Kingdom. Located in the City of Westminster, the palace is often at the centre of state occasions and royal hospitality.";
+    }
+    if ([self.detailTitle.text isEqualToString:@"London Eye"]) {
+        self.detailTextView.text = @"The London Eye, or the Millennium Wheel, is a cantilevered observation wheel on the South Bank of the River Thames in London. It is Europe's tallest cantilevered observation wheel, and the most popular paid tourist attraction in the United Kingdom with over three million visitors annually.";
+    }
+    if ([self.detailTitle.text isEqualToString:@"St Pauls Cathedral"]) {
+        self.detailTextView.text = @"St. Paul's Catholic Church is an American parish church of the Roman Catholic Archdiocese of San Francisco. The parish is located in the city of San Francisco, California, at 221 Valley Street and the corner of Church Street in the city's Noe Valley neighborhood.";
+    }
+    if ([self.detailTitle.text isEqualToString:@"Tower Bridge"]) {
+        self.detailTextView.text = @"The Tower Bridge is a vertical lift bridge across the Sacramento River, linking West Sacramento in Yolo County to the west, with the capital of California, Sacramento, in Sacramento County to the east. It has also been known as M Street Bridge.";
+    }
+    if ([self.detailTitle.text isEqualToString:@"Westminster Abbey"]) {
+        self.detailTextView.text = @"Westminster Abbey, formally titled the Collegiate Church of Saint Peter at Westminster, is an Anglican church in the City of Westminster, London, England. Since 1066, it has been the location of the coronations of 40 English and British monarchs, and a burial site for 18 English, Scottish, and British monarchs.";
+    }
 }
 
 /*
@@ -28,5 +54,8 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)directions:(id)sender {
+}
 
 @end
