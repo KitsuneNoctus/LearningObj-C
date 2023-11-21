84b7f422f1c8217e068d4c788e809f87db3d5a17
KitsuneNoctus <theceintiant@gmail.com>
2023-11-21 15:48:29 -0800

    Obj-C Universal Support - 3 - Different Device Storyboards START

---
+++ Section 13 - Universal Support/DeviceStoryboards/DeviceStoryboards/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  DeviceStoryboards
+//
+//  Created by Henry Calderon on 11/21/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Section 13 - Universal Support/DeviceStoryboards/DeviceStoryboards/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DeviceStoryboards
+//
+//  Created by Henry Calderon on 11/21/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
